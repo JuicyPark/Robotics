f8ecf9983d7253d12b00df48bfbb10f79271de7c
DevJuicy <bassoff@naver.com>
2020-04-09 01:50:52 +0900

    [#2] feat : 빈 슬롯에 정렬하여 장착

---
+++ Assets/Scripts/Frame.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Frame : MonoBehaviour
+{
+    [SerializeField] Frame[] sideFrames;
+
+    public void OnSideFrames()
+    {
+        foreach(var sideFrame in sideFrames)
+        {
+            if (sideFrame == null)
+                return;
+
+            if (!sideFrame.gameObject.activeSelf)
+                sideFrame.gameObject.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/Part.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Part : MonoBehaviour
+{
+    public Collider2D collider2D;
+}
